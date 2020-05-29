44d3c6863bf009bf4b2071dca4c451db4c3f8e19
theqin <1029589750@qq.com>
2020-05-29 09:16:14 +0800

    我又修改东西了

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@ using namespace std;
 int main()
 {
    cout<<"hello world"<<endl;
-   cout<<"what the fuck"<<endl;
-    return 0;[M E3
+   cout<<"what the fuck"<<endl;dfsasd
+    add1
+	   return 0;[M E3
 }
 
